e758a23fcc81e082df7ea7dea6cd77fa2972c402
Aaron García Cerqueiras <a.garcia7@udc.es>
2021-05-05 19:25:14 +0200

    Menú ppal arreglado

--- RoombaFighters/Assets/Scripts/Menus/ControladorOpciones.cs
+++ RoombaFighters/Assets/Scripts/Menus/ControladorOpciones.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControladorOpciones : MonoBehaviour
 {
-	public GameObject pantallaOpciones;
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Toggle togleMultijugador;
+
+    public Slider sliderTiempo;
+
+    public Text textoTiempo;
+
+    void Start(){
+        CargarConfiguracion();
+        UpdateTextoTiempo();  
+    }
+    public void UpdateTextoTiempo(){
+        textoTiempo.text = Mathf.RoundToInt(sliderTiempo.value).ToString("00 ") + "mins";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ConfiguracionPorDefecto(){
+        OpcionesJuego.GetInstance().tiempoMinutos = 5;
+        OpcionesJuego.GetInstance().multijugador = false;
+        CargarConfiguracion();
+    }
+    public void CargarConfiguracion(){
+        sliderTiempo.value = OpcionesJuego.GetInstance().tiempoMinutos;
+        togleMultijugador.isOn = OpcionesJuego.GetInstance().multijugador;
+    }
+    public void UpdateOpciones(){
+        OpcionesJuego.GetInstance().tiempoMinutos = Mathf.RoundToInt(sliderTiempo.value);
+        OpcionesJuego.GetInstance().multijugador = togleMultijugador.isOn;
     }
 }
